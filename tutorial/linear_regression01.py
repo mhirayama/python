c72a67ccdadb3bd3c72c80be598803b8f6a6a0b2
平山真 <hirayamamakoto@hirayamamakoto-no-MacBook-Pro.local>
2019-03-04 14:34:12 +0900

    tutorial

--- tutorial/linear_regression01.py
+++ tutorial/linear_regression01.py
@@ -1,17 +1,17 @@
 import numpy as np
-X = np.random.rand(100,1)
-y = 5 + 3 * X + np.random.rand(100, 1)
+X = np.random.rand(100,1) #乱数を生成
+y = 5 + 3 * X + np.random.rand(100, 1) #Xに対して線形的な乱数を生成
 
-from sklearn.linear_model import LinearRegression
-lin_reg = LinearRegression().fit(X, y.ravel())
-print(lin_reg.intercept_,  lin_reg.coef_)
+from sklearn.linear_model import LinearRegression #線形回帰を読み込み
+lin_reg = LinearRegression().fit(X, y.ravel()) #モデルの作成
+print(lin_reg.intercept_,  lin_reg.coef_) #切片と傾きを表示
 
-from sklearn.linear_model import SGDRegressor
-sgd_reg = SGDRegressor(max_iter = 100).fit(X, y.ravel())
-print(sgd_reg.intercept_, sgd_reg.coef_)
+from sklearn.linear_model import SGDRegressor #SDG回帰を読み込み
+sgd_reg = SGDRegressor(max_iter = 100).fit(X, y.ravel()) #モデルの作成
+print(sgd_reg.intercept_, sgd_reg.coef_) #切片と傾きを表示
 
-sgd_reg_00001 = SGDRegressor(eta0 = 0.0001, max_iter = 100).fit(X, y.ravel())
+sgd_reg_00001 = SGDRegressor(eta0 = 0.0001, max_iter = 100).fit(X, y.ravel()) #eta0を0.0001にして学習不足にして学習
 print(sgd_reg_00001.intercept_, sgd_reg_00001.coef_)
 
-sgd_reg_100 = SGDRegressor(eta0 = 100, max_iter = 100).fit(X, y.ravel())
+sgd_reg_100 = SGDRegressor(eta0 = 100, max_iter = 100).fit(X, y.ravel()) #eta0を100にして過学習にして学習
 print(sgd_reg_100.intercept_, sgd_reg_100.coef_)
--- tutorial/nearest_neigbor02.py
+++ tutorial/nearest_neigbor02.py
@@ -2,10 +2,14 @@ from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
+#データセットの読み込み、cancer.dataとcancer.targetをそれぞれX,yとする
 cancer = load_breast_cancer()
 
+#データの分割
 X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, stratify = cancer.target, random_state = 0)
 
+#モデルの作成
 clf = KNeighborsClassifier(n_neighbors = 3).fit(X_train, y_train)
 
+#正解率の計算
 print(round(clf.score(X_test, y_test), 3))
